4177b4d1049f40ae4d7e183b99e463100e437323
Khanstore <dmashraf@gmail.com>
2025-09-03 17:01:49 +0600

    Update sale_order.py manually

--- turbo_sale/models/sale_order.py
+++ turbo_sale/models/sale_order.py
@@ -4,6 +4,9 @@ from odoo import models, api, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    is_cash_on_delivery = fields.Boolean('Cash on Delivery')
+    cod_amount = fields.Float('Cash on Delivery Amount', help="Amount to be collected on delivery.")
+
     def action_confirm_all_with_prompts(self):
         for order in self:
             if order.state == 'draft':
@@ -17,7 +20,6 @@ class SaleOrder(models.Model):
                     if move.product_id.type == 'consu' and move.product_uom_qty > move.product_id.qty_available:
                         insufficient_products.append(move.product_id.display_name)
 
-            if insufficient_products:
                 return {
                     'name': 'Insufficient Stock',
                     'type': 'ir.actions.act_window',
@@ -28,10 +30,12 @@ class SaleOrder(models.Model):
                         'active_ids': self.ids,
                         'default_insufficient_products': ', '.join(insufficient_products),
                         'default_picking_id': picking.id,
+                        'default_cod_amount': order.amount_total,
+                        'default_carrier_id': picking.carrier_id.id,
                     }
                 }
 
-            return self._process_delivery_invoice()
+            # return self._process_delivery_invoice()
 
     def _process_delivery_invoice(self):
         for order in self:
@@ -56,7 +60,7 @@ class SaleOrder(models.Model):
                 }
             }
 
-    def _force_delivery_and_invoice(self):
+    def _force_delivery_and_invoice(self,data=None):
         for order in self:
             for picking in order.picking_ids.filtered(lambda p: p.state not in ('done', 'cancel')):
                 if picking.state == 'confirmed':
@@ -65,7 +69,7 @@ class SaleOrder(models.Model):
 
             invoice = order._create_invoices()
             invoice.action_post()
-
+            # return invoice.action_register_payment()
             return {
                 'name': 'Register Payment',
                 'type': 'ir.actions.act_window',
@@ -77,3 +81,40 @@ class SaleOrder(models.Model):
                     'active_ids': invoice.ids,
                 }
             }
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    is_cash_on_delivery = fields.Boolean('Cash on Delivery', default=False, help="Check if this picking is Cash on Delivery.")
+    cod_amount = fields.Float('Cash on Delivery Amount', help="Amount to be collected on delivery.")
+    tracking_url_link = fields.Html(string='Tracking Link', compute='_compute_tracking_url_link')
+
+    def _compute_tracking_url_link(self):
+        for picking in self:
+            if picking.carrier_tracking_ref and picking.carrier_id.tracking_url:
+                tracking_url = picking.carrier_id.tracking_url % picking.carrier_tracking_ref
+                picking.tracking_url_link = f'<a href="{tracking_url}" target="_blank">{picking.carrier_tracking_ref}</a>'
+            else:
+                picking.tracking_url_link = picking.carrier_tracking_ref or ''
+
+    def button_validate(self):
+        res = super(StockPicking, self).button_validate()
+        if self.is_cash_on_delivery:
+            self.message_post(body="This picking is confirmed as Cash on Delivery with amount: %s" % self.cod_amount)
+        return res
+
+# class CashOnDeliveryWizard(models.TransientModel):
+#     _name = 'cash.on.delivery.wizard'
+#     _description = 'Cash on Delivery Confirmation'
+#
+#     is_cash_on_delivery = fields.Boolean('Is this Cash on Delivery?')
+#     cod_amount = fields.Boolean('Is this Cash on Delivery?')
+#
+#     def action_confirm_cod(self):
+#         sale_orders = self.env['sale.order'].browse(self.env.context.get('active_ids'))
+#         sale_orders.write({'is_cash_on_delivery': self.is_cash_on_delivery})
+#         if self.is_cash_on_delivery:
+#             # You can send info to carrier here (e.g., update picking, tracking message, etc.)
+#             sale_orders.message_post(body="This order is confirmed as Cash on Delivery.")
+#
+#         return sale_orders._force_delivery_and_invoice(data)
